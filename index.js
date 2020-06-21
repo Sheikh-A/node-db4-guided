fc27b7ee698fe07ca18a8c7c5eb22e65920faa49
Ali Sheikh <ali.sheikh@flexport.com>
2020-06-20 19:00:39 -0700

    n

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
 });
+
+
+/*
+knex migrate:make create-tables
+
+select * from zoos as z
+join zoo_animals as za
+on z.id = za.zoo_id
+join animals as a 
+on a.id = za.animal_id
+
+*/
